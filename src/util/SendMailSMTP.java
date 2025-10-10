ed15b1338521a891e9dc32382068f82377a39fc2
NguyenPhucNhan <phucnhanpht0605@gmail.com>
2025-10-10 23:22:20 +0700

    util folder

---
+++ src/util/SendMailSMTP.java
@@ -0,0 +1,42 @@
+package util;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMailSMTP {
+
+    public static void sendOTP(String emailTo, String otp) {
+        String username = "phucnhanpht0605@gmail.com";
+        String password = "fimaqyvywnmqikch";
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailTo));
+            message.setSubject("Request to reset your password");
+            message.setText("Hi there!\n"
+                    + "\n"
+                    + "Someone has requested to reset the password for your account. If this was not you, please ignore this email.\n"
+                    + "\n"
+                    + "Use the following activation code to recover your password: " + otp);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/util/convertDate.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class convertDate {
+    public static convertDate getInstance() {
+        return new convertDate();
+    }
+
+    public Date ChangeFrom(Date date) throws ParseException {
+        SimpleDateFormat fm = new SimpleDateFormat("dd-MM-yyyy 00:00:00");
+        String dateText = fm.format(date);
+        SimpleDateFormat par = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        Date result = par.parse(dateText);
+        return result;
+    }
+
+    public Date ChangeTo(Date date) throws ParseException {
+        SimpleDateFormat fm = new SimpleDateFormat("dd-MM-yyyy 23:59:59");
+        String dateText = fm.format(date);
+        SimpleDateFormat par = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        Date result = par.parse(dateText);
+        return result;
+    }
+}

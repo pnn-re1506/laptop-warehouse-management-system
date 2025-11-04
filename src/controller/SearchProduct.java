74921d51db7db50785ce3028c8a8264b6b7c7a59
NguyenPhucNhan <phucnhanpht0605@gmail.com>
2025-11-04 22:53:04 +0700

    pakage controller

---
+++ src/controller/SearchProduct.java
@@ -0,0 +1,179 @@
+package controller;
+
+import dao.ComputerDAO;
+import model.Computer;
+
+import java.util.ArrayList;
+
+public class SearchProduct {
+    public static SearchProduct getInstance() {
+        return new SearchProduct();
+    }
+
+    public ArrayList<Computer> searchAll(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+                if (mt.getProductId().toLowerCase().contains(text.toLowerCase()) || mt.getProductId().toLowerCase().contains(text.toLowerCase())
+                        || mt.getCpuName().toLowerCase().contains(text.toLowerCase())
+                        || mt.getGraphicsCard().toLowerCase().contains(text.toLowerCase()))
+                        {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchProductId(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+                if (mt.getProductId().toLowerCase().contains(text.toLowerCase())) {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchProductName(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+                if (mt.getProductName().toLowerCase().contains(text.toLowerCase())) {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchQuantity(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+                if (text.length() != 0) {
+                    if (mt.getQuantity() > Integer.parseInt(text)) {
+                        result.add(mt);
+                    }
+                } else {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchImportPrice(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+
+                if (text.length() != 0) {
+                    if (mt.getImportPrice() > Integer.parseInt(text)) {
+                        result.add(mt);
+                    }
+                }
+                else {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchExportPrice(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStatus() == 1) {
+
+                if (text.length() != 0) {
+                    if (mt.getExportPrice() > Integer.parseInt(text)) {
+                        result.add(mt);
+                    }
+                }
+                else {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchRam(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getRam().toLowerCase().contains(text.toLowerCase())) {
+                result.add(mt);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchCpu(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getCpuName().toLowerCase().contains(text.toLowerCase())) {
+                result.add(mt);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchStorage(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getStorage().toLowerCase().contains(text.toLowerCase())) {
+                result.add(mt);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Computer> searchCard(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getGraphicsCard().toLowerCase().contains(text.toLowerCase())) {
+                result.add(mt);
+            }
+        }
+        return result;
+    }
+
+
+
+    public ArrayList<Computer> searchDeleted(String text) {
+        ArrayList<Computer> result = new ArrayList<>();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAll();
+        for (var mt : armt) {
+            if (mt.getStatus() == 0) {
+                if (mt.getProductId().toLowerCase().contains(text.toLowerCase())) {
+                    result.add(mt);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Computer searchId(String text) {
+        Computer result = new Computer();
+        ArrayList<Computer> armt = ComputerDAO.getInstance().selectAllExist();
+        for (var mt : armt) {
+            if (mt.getProductId().toLowerCase().contains(text.toLowerCase())) {
+                return mt;
+            }
+        }
+        return null;
+    }
+}
